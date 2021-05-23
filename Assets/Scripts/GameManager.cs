c1f68183154742f25c35892ccac463e885f779ea
MK <sirevall@gmail.com>
2021-05-24 02:00:17 +0700

    Created a basic template from UI. Created a timer script. Made wheat production on a timer. Added the ability to recruit warriors.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public int peasants;
+    public int warriors;
+    public int wheat;
+    public int peasantPrice;
+    public int warriorPrice;
+    public int peasantProduce;
+    public int warriorConsume;
+    public float wheatProductionTime;
+
+    public Text infoText;
+
+    public Timer wheatProductionTimer;
+
+    private int _dead = 0;
+
+    private void Awake()
+    {
+        wheatProductionTimer.time = wheatProductionTime;
+    }
+
+    private void Start()
+    {
+        
+        PrintInfo();
+    }
+
+    private void Update()
+    {
+        PrintInfo();
+        if (wheatProductionTimer.timeOver)
+            CreateWheat();
+    }
+
+    public void CreateWarrior()
+    {
+        if (wheat > 0 && (wheat - warriorPrice) > 0)
+        {
+            ++warriors;
+            wheat -= warriorPrice;
+        }
+        
+    }
+    private void CreateWheat()
+    {
+        wheat += (peasants * peasantProduce);
+    }
+
+    private void PrintInfo()
+    {
+        infoText.text = $"Воины: {warriors}\n\n Крестьяне: {peasants}\n\n Пшеница: {wheat}\n\n Погибло: {_dead}";
+    }
+}
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    public float time;
+    public GameObject timerImage;
+    public bool timeOver;
+    private float _timeLeft;
+
+    void Start()
+    {
+        _timeLeft = time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Tick();
+        CheckTimerStatus();
+    }
+
+    private void Tick()
+    {
+        _timeLeft -= Time.deltaTime;
+    }
+    private void CheckTimerStatus()
+    {
+        if (_timeLeft > 0)
+        {
+            timeOver = false;
+        }
+        else
+        {
+            _timeLeft = time;
+            timeOver = true;
+        }
+    }
+}
